3b7cbedbe4317cbf99812783c779b1421a947aed
EaleksS <ealgenv@gmail.com>
2023-05-26 22:58:20 +0900

    .

--- src/app.module.ts
+++ src/app.module.ts
@@ -11,12 +11,18 @@ import { join } from 'path';
       type: 'postgres',
       host: 'ep-weathered-breeze-927410-pooler.eu-central-1.postgres.vercel-storage.com',
       port: 5432,
+      url: 'postgres://default:dPLwFtBz0rn6@ep-weathered-breeze-927410-pooler.eu-central-1.postgres.vercel-storage.com:5432/verceldb',
       database: 'verceldb',
       username: 'default',
       password: 'dPLwFtBz0rn6',
       entities: [join(__dirname, '**', '*.entity{.ts,.js}')],
       migrations: [join(__dirname, '**', '*.migration{.ts,.js}')],
       synchronize: true,
+      extra: {
+        ssl: {
+          rejectUnauthorized: false
+        }
+      }
     }),
     PhoneModule,
   ],
